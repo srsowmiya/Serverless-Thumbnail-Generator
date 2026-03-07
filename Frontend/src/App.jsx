ad046273b338681329bcfca60452b50adb2555b8
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-03-07 13:20:50 +0530

    Frontend call is defined and handled

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,34 +3,76 @@ import './App.css';
 
 const App = () => {
   const [file, setFile] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
 
   const handleFile = (e) => {
     const selectedFile = e.target.files[0];
     if (selectedFile) {
       setFile(selectedFile);
+      handleUpload(selectedFile);
+    }
+  };
+
+  const handleUpload = async (fileToUpload) => {
+    
+    if (!fileToUpload) return;
+    
+    setIsUploading(true);
+
+    try {
+     
+      const response = await fetch(import.meta.env.VITE_API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          filename: fileToUpload.name,
+          filetype: fileToUpload.type
+        })
+      });
+
+      if (!response.ok) throw new Error('Failed to get the upload url');
+      
+      const data = await response.json();
+      
+     
+      const s3Url = data.uploadUrl; 
+
+   
+      const uploadResponse = await fetch(s3Url, {
+        method: 'PUT',
+        headers: { 'Content-Type': fileToUpload.type },
+        body: fileToUpload
+      });
+
+      if (uploadResponse.ok) {
+        alert('File uploaded successfully!');
+      } else {
+        console.error('S3 Upload Failed');
+      }
+    } catch (err) {
+      console.error('Error:', err);
+    } finally {
+      setIsUploading(false);
     }
   };
 
   return (
     <div className="main-wrapper">
-      
       <div className="left-side">
         <div className="content-box">
           <h1>Create Stunning Thumbnails</h1>
-          <p>
-            Process your media through our high-speed serverless 
-            infrastructure. Simple, fast, and scalable.
-          </p>
+          <p>Simple, fast, and scalable serverless infrastructure.</p>
           
           <div className="upload-container">
             <label htmlFor="file-upload" className="shiny-button">
-              {file ? "Change File" : "Upload Media"}
+              {isUploading ? "Uploading..." : (file ? "Change File" : "Upload Media")}
             </label>
             <input 
               type="file" 
               id='file-upload' 
               onChange={handleFile} 
               accept="image/*,video/*" 
+              disabled={isUploading}
             />
           </div>
 
@@ -42,11 +84,7 @@ const App = () => {
           )}
         </div>
       </div>
-
-      {/* Right Side: 60% width with Background Image */}
-      <div className="right-side">
-        {/* The background is handled in CSS */}
-      </div>
+      <div className="right-side"></div>
     </div>
   );
 }
